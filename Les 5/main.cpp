09859e4b461e9570f84ffbc236f3d7bfac83c522
Adriaan van Oostveen Laptop <adriaanvanoostveen@gmail.com>
2018-10-18 10:35:40 +0200

    notes les 5

---
+++ Les 5/main.cpp
@@ -0,0 +1,21 @@
+
+//when self managing memory, always have the big three: Copy-constructor, Destructor, and Assignment operator
+
+//copy constructor
+//Broodmandje(const Broodmantje& b);
+
+//assignment operator
+//Broodmantje& operator=(const Broodmantje& b);
+
+//Assignment: make a sad book, like the sad bread basket
+//- make a book on the heap
+//- make it possible for a book to be borrowed
+//- multiple books
+
+int main()
+{
+
+
+
+	return 0;
+}
